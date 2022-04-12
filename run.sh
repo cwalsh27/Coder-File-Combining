7fc66e2723151109ee0a8014859abbc77b3c1e53
Tyler Hecht <50639776+Tyler-Hecht@users.noreply.github.com>
2022-04-12 15:25:39 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-python3 clearer.py
+python3 clearer.py 
 if [ $? -ne 0 ]; then exit 0; fi 
-python3 input_mover.py
+python3 input_mover.py 
 if [ $? -ne 0 ]; then exit 0; fi 
-cd DatavyuToSupercoder
-java -jar DatavyuToSupercoder.jar
-cd ..
-python3 copier.py
+cd DatavyuToSupercoder 
+java -jar DatavyuToSupercoder.jar 
+cd .. 
+python3 copier.py 
 if [ $? -ne 0 ]; then exit 0; fi 
-cd Facetalk
-python3 catcher.py
+cd Facetalk 
+python3 catcher.py 
 if [ $? -ne 0 ]; then exit 0; fi 
-python3 facetalk.py
+python3 facetalk.py 
 if [ $? -ne 0 ]; then exit 0; fi 
-cd ..
-python3 output_mover.py
+cd .. 
+python3 output_mover.py 
 if [ $? -ne 0 ]; then exit 0; fi 
-pause
