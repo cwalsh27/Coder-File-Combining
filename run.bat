78e7820c9c5ab6e2052f0d671e13b03e88dca031
Tyler Hecht <50639776+Tyler-Hecht@users.noreply.github.com>
2022-04-12 14:38:12 -0400

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,13 +1,31 @@
 @echo off
 py clearer.py
+if %errorlevel% neq 0 (
+	pause
+	exit)
 py input_mover.py
+if %errorlevel% neq 0 (
+	pause
+	exit)
 cd DatavyuToSupercoder
 java -jar DatavyuToSupercoder.jar
 cd ..
 py copier.py
+if %errorlevel% neq 0 (
+	pause
+	exit)
 cd Facetalk
 py catcher.py
+if %errorlevel% neq 0 (
+	pause
+	exit)
 py facetalk.py
+if %errorlevel% neq 0 (
+	pause
+	exit)
 cd ..
 py output_mover.py
+if %errorlevel% neq 0 (
+	pause
+	exit)
 pause
