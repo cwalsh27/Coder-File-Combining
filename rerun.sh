48419e429e56c8c1e7a0c1c569eeffbd30ce5389
Tyler Hecht <50639776+Tyler-Hecht@users.noreply.github.com>
2022-04-21 14:14:35 -0400

    Add files via upload

--- rerun.sh
+++ rerun.sh
@@ -3,7 +3,13 @@ python3 rerunner.py True
 if [ $? -ne 0 ]; then exit 0; fi 
 cd Facetalk 
 python3 catcher.py 
-if [ $? -ne 0 ]; then exit 0; fi 
+if [ $? -ne 0 ] 
+then 
+	python3 facetalk.py 
+	cd .. 
+	python3 rerunner.py False 
+	exit 0 
+fi 
 python3 facetalk.py 
 if [ $? -ne 0 ]; then exit 0; fi 
 cd .. 
--- run.sh
+++ run.sh
@@ -4,13 +4,19 @@ if [ $? -ne 0 ]; then exit 0; fi
 python3 input_mover.py 
 if [ $? -ne 0 ]; then exit 0; fi 
 cd DatavyuToSupercoder 
-java -jar DatavyuToSupercoder.jar
+java -jar $(pwd)/DatavyuToSupercoder.jar
 cd .. 
 python3 copier.py 
 if [ $? -ne 0 ]; then exit 0; fi 
 cd Facetalk 
 python3 catcher.py 
-if [ $? -ne 0 ]; then exit 0; fi 
+if [ $? -ne 0 ] 
+then 
+	python3 facetalk.py 
+	cd .. 
+	python3 output_mover.py 
+	exit 0 
+fi 
 python3 facetalk.py 
 if [ $? -ne 0 ]; then exit 0; fi 
 cd .. 
