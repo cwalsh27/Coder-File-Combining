d39ab3094578b26ebe994836f3b30e581e8b4ff4
Tyler Hecht <50639776+Tyler-Hecht@users.noreply.github.com>
2022-04-26 14:47:25 -0400

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo Note: this file should only be run once per set of coder files
+echo If you want to edit the output and run again, use rerun.bat
+echo --------------------------------------------------------------
 python3 clearer.py 
 if [ $? -ne 0 ]; then exit 0; fi 
 python3 input_mover.py 
@@ -11,7 +14,8 @@ if [ $? -ne 0 ]; then exit 0; fi
 cd Facetalk 
 python3 catcher.py 
 if [ $? -ne 0 ] 
-then  
+then
+	echo 
 	cd .. 
 	python3 output_mover.py 
 	exit 0 
