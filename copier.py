30772732ba7662c31a1139abf68034c4237f1a80
Tyler Hecht <50639776+Tyler-Hecht@users.noreply.github.com>
2022-05-03 16:14:27 -0400

    Update copier.py

--- copier.py
+++ copier.py
@@ -12,18 +12,11 @@ input_path = path + "/DatavyuToSupercoder/Output"
 output_path = path + "/combining/Input"
 files = glob.glob(os.path.join(input_path, "*.xls"))
 
-# gets rid of that weird OUTPUT.DS_S file
-real_files = []
-for file in files:
-    file_name = file.split(sep)[-1]
-    if file_name != "OUTPUT_.DS_S.xls":
-        real_files.append(file_name)
-
 # gets the relevant parts of the data
 os.chdir(input_path)
 dataframes = []
 coders = []
-for file in real_files:
+for file in files:
     df = pandas.read_excel(file, skiprows = [0], na_filter = True)
     df = df[["Code", "Onset", "Offset"]]
     dataframes.append(df)
@@ -31,7 +24,7 @@ for file in real_files:
     coders.append(coder)
 
 # creates the .xlsx files
-name = "_".join(real_files[0].split("_")[1:-1])
+name = "_".join(files[0].split("_")[1:-1])
 os.chdir(output_path)
 with pandas.ExcelWriter(name+'.xlsx') as writer:  
     dataframes[0].to_excel(writer, sheet_name='Coder 1 ' + coders[0], index = False, header = False)
